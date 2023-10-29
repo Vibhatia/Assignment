74472180915c619b492f313931ebd658bc346561
Vibhatia <bhatiavikas2001@gmail.com>
2023-10-29 17:01:28 +0530

    Navbar created

---
+++ src/components/Kanban.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+const Kanban = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Kanban
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles/Navbar.css'
+
+const settingsIcon = '/icons/settings.svg'
+const chevronIcon = '/icons/chevron-down.svg'
+const Navbar = () => {
+  return (
+   <>
+      Navbar
+
+   </>
+  )
+}
+
+export default Navbar
